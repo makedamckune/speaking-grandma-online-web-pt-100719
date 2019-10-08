71b36d694791f5f0d7f0620746259beb3a9304a0
Makeda P Mckune <makedamckune@gmail.com>
2019-10-08 21:22:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,10 @@ def speak_to_grandma
   talking_to = "Grandma"
 if I say, I love you Grandma
   puts "I love you too Pumpkin".upcase
-elsif I say Hi nana, 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
+elsif I say Hi nana, how are you?
+   puts "Huh? Sonny Speak up".upcase
+elsif I shout 
+    puts "Not "
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
