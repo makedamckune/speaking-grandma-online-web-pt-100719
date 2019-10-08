3b2c1cda2587867a64778f1c2ded21b5089b4eac
Makeda P Mckune <makedamckune@gmail.com>
2019-10-08 21:19:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma
   talking_to = "Grandma"
 if I say, I love you Grandma
   puts "I love you too Pumpkin".upcase
-elsif I say, 
+elsif I say Hi nana, 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
