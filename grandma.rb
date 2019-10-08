33e509b03931218a0d86532f5c821c87e7d4bf60
Makeda P Mckune <makedamckune@gmail.com>
2019-10-08 21:16:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma
   talking_to = "Grandma"
-if "I say, I love you Grandma"
-  puts 
+if I say, I love you Grandma
+  puts ""
 # unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so) 
